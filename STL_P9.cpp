a773206b49994e7f8c89cf8b0957f69f42a1eed2
NE420 <119597028+NE420@users.noreply.github.com>
2026-07-19 20:38:59 +0300

    Create STL_P9.cpp
    Implement a program to extract and sort distinct words from input text, ignoring case.

---
+++ STL_P9.cpp
@@ -0,0 +1,112 @@
+/*
+Andy, 8, has a dream - he wants to produce his
+very own dictionary. This is not an easy task for
+him, as the number of words that he knows is,
+well, not quite enough. Instead of thinking up all
+the words himself, he has a briliant idea. From
+his bookshelf he would pick one of his favourite
+story books, from which he would copy out all
+the distinct words. By arranging the words in
+alphabetical order, he is done! Of course, it is
+a really time-consuming job, and this is where a
+computer program is helpful.
+You are asked to write a program that lists
+all the different words in the input text. In this
+problem, a word is de ned as a consecutive se-
+quence of alphabets, in upper and/or lower case.
+Words with only one letter are also to be considered. Furthermore, your program must be CaSe In-
+SeNsItIvE. For example, words like \Apple", \apple" or \APPLE" must be considered the same.
+Input
+The input le is a text with no more than 5000 lines. An input line has at most 200 characters. Input
+is terminated by EOF.
+Output
+Your output should give a list of different words that appears in the input text, one in a line. The
+words should all be in lower case, sorted in alphabetical order. You can be sure that he number of
+distinct words in the text does not exceed 5000.
+Sample Input
+Adventures in Disneyland
+Two blondes were going to Disneyland when they came to a fork in the
+road. The sign read: "Disneyland Left."
+So they went home.
+Sample Output
+a
+adventures
+blondes
+came
+disneyland
+fork
+going
+home
+in
+left
+read
+road
+sign
+so
+the
+they
+to
+two
+went
+were
+when
+*/
+
+#include <iostream>
+#include <vector>
+#include <deque>
+#include <set>
+#include <map>
+#include <algorithm>
+#include <string>
+#include <cmath>
+
+using namespace std;
+
+
+int main()
+{
+	cin.tie(nullptr), cout.tie(nullptr), cin.sync_with_stdio(false), cout.sync_with_stdio(false);
+
+	set<string> dict;
+
+		string line;
+		string word = "";
+
+		while (getline(cin, line) && !cin.eof())
+		{
+			for (int i = 0; i < line.size(); i++)
+			{
+				if (line[i] != ' '  && (line[i] >= 'a' && line[i] <= 'z') || (line[i] >= 'A' && line[i] <= 'Z')) {
+					word += line[i];
+				}
+				else {
+					for (auto& i : word) {
+						i = tolower(i);
+					}
+					dict.insert(word);
+					word = "";
+				}
+				if (i == line.size()-1) {
+					for (auto& i : word) {
+						i = tolower(i);
+					}
+					dict.insert(word);
+					word = "";
+				}
+			}		
+		}
+
+	for (auto i : dict) {
+		if (i == "" || i == "\n") {
+			continue;
+		}
+		
+		cout << i  << endl;
+		
+	}
+
+
+	return 0; 
+
+}
